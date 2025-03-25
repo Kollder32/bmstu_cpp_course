7293e8d9866abe5238104c81ecb355ee79a4dbec
dmitrytsyzBMSTU <dmitrytsygankov9000@mail.ru>
2025-03-25 12:04:23 +0300

    вернул в hw basic_c

--- tasks/task_basic_c/task_str2int/str2int.c
+++ tasks/task_basic_c/task_str2int/str2int.c
@@ -1,8 +1,29 @@
-#include "str2int.h"
-#include <assert.h>
 #include "stdio.h"
+#include <assert.h>
+#include "str2int.h"
 
-int str2int(const char* str)
-{
-	return 0;
+int str2int(const char *str) {
+    int zn = 1;
+    int res = 0;
+
+    if (*str == '-'){
+        zn = -1;
+        str++;
+    } else if (*str == '+') {
+        str++;
+    }
+
+    int count = 0;
+    while (*str != '\0') {
+        res = res * 10 + (*str - '0');
+        count += 1;
+        str++;
+        assert(zn == -1 && res == -2147483648 || zn == -1 && res >= 0 && count <= 9 || zn == 1 && res >= 0);
+    }
+    assert(count > 0);
+    return res * zn;
 }
+
+
+
+
